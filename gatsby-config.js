bbd3b604a8dffd29b3b553e47fcf98879e971dc8
LiHS <lihsai0@gmail.com>
2020-12-29 20:46:32 +0800

    chore(gatsby-config): change style, update information, some comment

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,20 +1,16 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `Lihs.me`,
+    description: `A part of good time in life.`,
+    author: `@lihs`,
   },
   plugins: [
-    `gatsby-plugin-react-helmet`,
-    {
-      resolve: `gatsby-source-filesystem`,
-      options: {
-        name: `images`,
-        path: `${__dirname}/src/images`,
-      },
-    },
-    `gatsby-transformer-sharp`,
-    `gatsby-plugin-sharp`,
+    // ## 构建相关
+    /*
+    * manifest 文件生成，PWA 说明文件的一部分
+    * 允许用户将网站作为应用添加到主屏幕上
+    * https://www.gatsbyjs.com/plugins/gatsby-plugin-manifest/
+    * */
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
@@ -27,8 +23,46 @@ module.exports = {
         icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
       },
     },
-    // this (optional) plugin enables Progressive Web App + Offline functionality
-    // To learn more, visit: https://gatsby.dev/offline
+
+    /*
+    * this (optional) plugin enables Progressive Web App + Offline functionality
+    * To learn more, visit: https://gatsby.dev/offline
+    * https://www.gatsbyjs.com/plugins/gatsby-plugin-offline/
+    * note: this plugin should be *after* gatsby-plugin-manifest
+    * */
     // `gatsby-plugin-offline`,
+
+    // ## GraphQL 数据源相关
+    /*
+    * 本地文件的读取，可供 GraphQL 使用
+    * https://www.gatsbyjs.org/packages/gatsby-source-filesystem/
+    * */
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `images`,
+        path: `${__dirname}/src/images`,
+      },
+    },
+
+    /*
+    * 为 GraphQL 提供了访问图片的能力
+    * https://www.gatsbyjs.org/packages/gatsby-transformer-sharp/
+    * */
+    `gatsby-transformer-sharp`,
+
+    // ## 页面元素相关
+    /*
+    * 添加 HTML <head> 内的常用元素，进而做到 SEO 等
+    * 示例见 src/components/seo.js
+    * https://www.gatsbyjs.org/packages/gatsby-plugin-react-helmet/
+    * */
+    `gatsby-plugin-react-helmet`,
+
+    /*
+    * 用于处理图片，其本质上是使用了 https://github.com/lovell/sharp 这个库
+    * https://www.gatsbyjs.org/packages/gatsby-plugin-sharp/
+    * */
+    `gatsby-plugin-sharp`,
   ],
 }
